b9d86335d80c7e293b5e0b048986be35bc593559
XojeDesktop <dawxoje@gmail.com>
2022-01-23 16:59:53 +0100

    Fix express performace 2

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const app_name = require('./package.json').name;
-console.log(app_name);
-const main_path = path.join(__dirname, 'src');
+const app_name = "Frontend-Final";
+
 // Serve static files
-app.use(express.static(`${__dirname}/dist/${app_name}`));
+app.use(express.static(__dirname + '/dist/' + app_name));
 
 // Send all requests to index.html
 app.get('/*', function (req, res) {
-	res.sendFile(path.join(`${__dirname}/dist/${app_name}/index.html`));
+	res.sendFile(path.join(__dirname + '/dist/' + app_name + '/index.html'));
 });
 
 // default Heroku port
