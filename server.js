6ca6015b6d9b311291035f9ecd540bd1e40fbe91
Xoje <dawxoje@gmail.com>
2022-04-13 18:28:53 +0200

    Final Deploy 3

--- server.js
+++ server.js
@@ -1,36 +1,34 @@
-const path = require('path');
-const express = require('express');
-const { env } = require('process');
-const cors = require('cors');
+const path = require("path");
+const express = require("express");
+const { env } = require("process");
+const cors = require("cors");
 const app = express();
-const PORT = 5000;// default Heroku port
+const PORT = 5000; // default Heroku port
 const app_name = "frontend-final";
 //const folder_path = `${__dirname}/dist/${app_name}`;
 const folder_path = `${__dirname}/dist/${app_name}`;
-
+const corsOptions = {
+	origin: ["https://back-final-xoje.herokuapp.com/"],
+	preflightContinue: false,
+	credentials: true,
+};
 
 // Serve static files
 app.use(express.static(folder_path));
-app.use(cors())
+app.use(cors(corsOptions));
 
 // Send all requests to index.html
-app.get('/*', function (req, res) {
+app.get("/*", function (req, res) {
 	/* res.set('Access-Control-Allow-Origin', "https://back-final-xoje.herokuapp.com");
 	res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Credentials", "true");
 	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 	res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"); */
-	res.sendFile(path.join(folder_path + '/index.html'));
-
+	res.sendFile(path.join(folder_path + "/index.html"));
 });
-app.options('*', cors());
+app.options("*", cors());
 
 const port = process.env.PORT || PORT;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
-
-
