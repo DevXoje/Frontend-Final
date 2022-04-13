2abcb11c736687d1fc365cc833a0e62db1d11535
Xoje <dawxoje@gmail.com>
2022-04-13 17:24:27 +0200

    Deploy 4

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ const app_name = "Frontend-Final";
 
 // Serve static files
 app.use(express.static(__dirname + '/dist/' + app_name));
-app.use(cors())
+//app.use(cors())
 
 // Send all requests to index.html
 app.get('/*', function (req, res) {
-	res.set('Access-Control-Allow-Origin', "https://back-final-xoje.herokuapp.com");
+	/* res.set('Access-Control-Allow-Origin', "https://back-final-xoje.herokuapp.com");
 	res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Credentials", "true");
 	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-	res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+	res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"); */
 	res.sendFile(path.join(__dirname + '/dist/' + app_name + '/index.html'));
-	
+
 });
 
 const port = process.env.PORT || PORT;
