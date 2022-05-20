b687431e38a4ce792e498e0241dc4bf6342d409e
Xoje <dawxoje@gmail.com>
2022-05-20 10:37:39 +0200

    Fix envirionment bug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
 	production: false,
 	stripePK: 'pk_test_51KQAFcDPCG8aXhaq3uC0Ut0opcSqtodqp8TVZxAzAnCk9QP34TdRouphcQjtiiOZphNxyXnW22Q3gDwPF6B9zMo5002IVClPpo',
-	//baseUrl: 'http://localhost:8000/api',
-	baseUrl: 'https://back-final-xoje.herokuapp.com/api'
+	baseUrl: 'http://localhost:8000/api',
 };
 
 /*
